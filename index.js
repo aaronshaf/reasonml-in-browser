744fb46f4f921739a32fd37db4e0e077a1d055c0
Aaron Shafovaloff <aaronshaf@gmail.com>
2017-12-26 18:47:04 -0700

    fix

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
+// getscript
+
+function getscript(uri, cb) {
+  if (!uri) throw 'missing uri'
+  var head = document.head || document.head.getElementsByTagName('head')[0]
+  var el = document.createElement('script')
+  el.type = 'text/javascript'
+  if ('function' === typeof cb) {
+    el.onerror = onerror.bind(null, uri, cb)
+    el.onload = onload.bind(null, uri, cb)
+  }
+  head.appendChild(el)
+  el.src = uri
+}
+
+function onerror(uri, cb, e) {
+  cb(new URIError(e.target.src + ' could not be loaded'), e)
+}
+
+function onload(uri, cb, e) {
+  cb(null, { uri: uri, event: e })
+}
+
+// end getscript
+
 function evaluateReasonML(code) {
   const ast = window.parseRE(code)
   const ocaml = window.printML(ast)
